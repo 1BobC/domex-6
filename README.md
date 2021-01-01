3e3ca229c7022d1a71f9513e2c7d8638d8229b4f
Bob.C <bob.cocksedge@gmail.com>
2021-01-01 13:16:46 +0000

    Do query selector exercises to 15min mark in video #6

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # domex-6
+"Create repo domex-6 with README.md Add index.html and jscript.js"
+"Do query selector exercises to 15min mark in video #6 "
---
+++ jscript.js
@@ -0,0 +1,25 @@
+// const para = document.querySelector('p');
+// console.log(para);
+
+// const para = document.querySelector('.error');
+// console.log(para);
+
+// const para = document.querySelector('div.error');
+// console.log(para);
+
+//  const para = document.querySelector('body > h3:nth-child(1)');
+//  console.log(para);
+
+//  const para = document.querySelector('div.error:nth-child(6)');
+//   console.log(para); 
+
+//  const paras = document.querySelectorAll('p');
+//  paras.forEach(para => {
+//     console.log(para);    
+//  });
+//    console.log(paras); 
+//    console.log(paras[4]);
+
+// const errors= document.querySelectorAll('.error');
+// console.log(errors);
+// start 15.21
