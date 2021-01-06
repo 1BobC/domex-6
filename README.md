1ad4a96517c1b7ba7207cea78a92d089981d7879
Bob.C <bob.cocksedge@gmail.com>
2021-01-06 18:09:57 +0000

    Complete udemy 52 in jscriptA.js and start 53

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 "Do query selector exercises to 15min mark in video #6 "
 "In jscript.js complete up to udemy 49 getting and setting attributes Add jscriptA.js for udemy 50 changing CSS styles on"
 "Add stylesheet.CSS for udemy 51 addng and removing classes"
+"Complete udemy 52 in jscriptA.js and start 53"
--- jscriptA.js
+++ jscriptA.js
@@ -9,4 +9,29 @@
 // title.style.margin= '';
 
 //udemy 51 addng and removing classes
+//  const content= document.querySelector('p');
+//  console.log(content.classList);
+//  content.classList.add('error');
+//  content.classList.remove('error');
+//  content.classList.add('success');
 
+//challange
+//  const contents= document.querySelectorAll('p')
+//  console.log(contents);  
+//     //console.log(item.innerText);                  use textContent not innerText why?
+//     //item.innerText += "  have I found you?";                     
+//  contents.forEach(item =>{
+ 
+//      console.log(item.textContent);
+//     if(item.textContent.includes('error')){         //alternating use of innerText
+//         item.classList.add('error');                // and  textContent             why?
+//     }if(item.innerText.includes('success')){
+//         item.classList.add('success');
+//     }
+//  });
+        // and now - how to toggle classes!
+// const title= document.querySelector('.title');
+// title.classList.toggle('test');
+// title.classList.toggle('test');
+
+//Start 53 Event Basics - click events
